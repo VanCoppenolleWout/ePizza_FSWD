9cb4a5a01fcd11eaa405243c5fcb060c91b3d9af
Glenn <glenn.troncquo@student.howest.be>
2021-10-22 14:30:48 +0200

    Role Authorisation Added

--- Backend/src/app.controller.ts
+++ Backend/src/app.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common'
 import { AppService } from './app.service'
 import { FirebaseAuthGuard } from './firebase/firebase-auth.guard'
+import { RolesGuard } from './firebase/roles.guard'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('firebase')
-  @UseGuards(FirebaseAuthGuard)
+  @UseGuards(FirebaseAuthGuard, RolesGuard)
   get(@Req() req) {
-    
     return req.user
   }
 }
