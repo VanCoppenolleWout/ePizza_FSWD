d0afb60b07c58c9927075cd41cbf897e2dec3abd
Glenn <glenn.troncquo@student.howest.be>
2021-12-01 19:11:32 +0100

    footer update

--- Frontend/ePizza/src/components/AppFooter.vue
+++ Frontend/ePizza/src/components/AppFooter.vue
@@ -12,36 +12,67 @@ export default defineComponent({
     style="background-color: #f1f0f2"
   >
     <div class="flex flex-col justify-between space-y-10">
-      <div class="items-start flex flex-row justify-between flex-wrap">
+      <div
+        class="
+          items-start
+          flex flex-col
+          sm:flex-row
+          justify-between
+          sm:flex-wrap
+        "
+      >
         <div class="text-3xl text-p-gray font-bold mx-2 my-2">ePizza</div>
-        <div class="flex flex-row space-x-20">
+        <div class="sm:flex sm:flex-row sm:space-x-10 md:space-x-20">
           <div class="mx-2 my-2">
             <h3 class="text-2xl font-medium">About us</h3>
-            <p>Why ePizza?</p>
-            <p>Services</p>
-            <p>Menu</p>
+            <a href="#header" class="hover:underline cursor-pointer block"
+              >Why ePizza?</a
+            >
+            <a href="#services" class="hover:underline cursor-pointer block"
+              >Services</a
+            >
+            <router-link to="/menu" class="hover:underline cursor-pointer block"
+              >Menu</router-link
+            >
           </div>
           <div class="mx-2 my-2">
             <h3 class="text-2xl font-medium">Follow us</h3>
-            <p>Why ePizza?</p>
-            <p>Services</p>
-            <p>Menu</p>
+            <a
+              href="#"
+              class="hover:underline cursor-pointer flex items-center"
+            >
+              Instagram
+            </a>
+            <a href="#" class="hover:underline cursor-pointer block"
+              >Facebook</a
+            >
+            <a href="#" class="hover:underline cursor-pointer block">Twitter</a>
           </div>
 
           <div class="mx-2 my-2">
             <h3 class="text-2xl font-medium">Legal</h3>
-            <p>Why ePizza?</p>
-            <p>Services</p>
-            <p>Menu</p>
+            <a href="#" class="hover:underline cursor-pointer block"
+              >Privacy Policy</a
+            >
+            <a href="#" class="hover:underline cursor-pointer block">Cookies</a>
+            <a href="#" class="hover:underline cursor-pointer block"
+              >General Conditions</a
+            >
           </div>
           <div class="mx-2 my-2">
             <h3 class="text-2xl font-medium">Contact info</h3>
-            <p>Why ePizza?</p>
-            <p>Services</p>
-            <p>Menu</p>
+
+            <a
+              href="mailto:info@pizza.com"
+              class="hover:underline cursor-pointer block"
+              >info@pizza.com</a
+            >
+            <a href="tel:055808420" class="hover:underline cursor-pointer block"
+              >055 80 84 20</a
+            >
+            <!-- <a href="#" class="hover:underline cursor-pointer block">Menu</a> -->
           </div>
         </div>
-        <div></div>
       </div>
 
       <div class="flex flex-row justify-between flex-wrap">
@@ -50,18 +81,18 @@ export default defineComponent({
         </p>
         <div class="flex flex-row items-center space-x-2">
           <svg
+            class="stroke-current text-gray-700"
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
             viewBox="0 0 24 24"
             fill="none"
-            stroke="#000000"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           >
-            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            <path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9" />
+            <path d="M9 22V12h6v10M2 10.6L12 2l10 8.6" />
           </svg>
           <p>Graaf Karel de Goedelaan 6 | 8500 Kortrijk</p>
         </div>
--- Frontend/ePizza/src/screens/Landingpage.vue
+++ Frontend/ePizza/src/screens/Landingpage.vue
@@ -58,211 +58,227 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="container mx-auto p-8 md:px-0">
-    <BannerComponent v-if="userRegistered" @close="closeTab" />
-    <AppHeader mode="full" />
+  <div>
+    <div class="container mx-auto p-8 md:px-0">
+      <BannerComponent v-if="userRegistered" @close="closeTab" />
+      <AppHeader mode="full" />
 
-    <div class="mt-32">
-      <img
-        class="
-          absolute
-          pizza-image
-          hidden
-          md:block
-          w-input
-          pointer-events-none
-          transition-all
-          ease
-          duration-300
-          img
-        "
-        src="../assets/images/pizza-bg.png"
-        alt=""
-      />
-
-      <header class="m-w-top">
-        <h1 class="font-semibold text-6xl animation1">
-          Newest, freshest and best pizza in
-          <span class="text-red-500">Kortrijk</span>.
-        </h1>
-        <h2 class="font-medium text-2xl pt-10 animation1">
-          Always wanted to try the freshest pizza in the whole wide world? Wait
-          no longer and taste for
-          <span class="text-red-500">youself</span>.
-        </h2>
-        <div class="flex flex-row items-center space-x-5 mt-6 animation1">
-          <button
-            class="rounded-xl bg-yellow-300 py-4 w-40 text-white font-semibold"
-            @click="handleDelivery"
-          >
-            DELIVERY
-          </button>
-          <p class="font-semibold">OR</p>
-          <button
-            class="rounded-xl bg-yellow-300 py-4 w-40 text-white font-semibold"
-            @click="handleCarryout"
-          >
-            CARRYOUT
-          </button>
-        </div>
-      </header>
-      <div class="m-w-landing" id="services">
-        <div
+      <div class="mt-32">
+        <img
           class="
-            grid grid-cols-1
-            lg:grid-cols-2
-            gap-y-10
-            justify-items-stretch
-            pt-24
+            absolute
+            pizza-image
+            hidden
+            md:block
+            w-input
+            pointer-events-none
+            transition-all
+            ease
+            duration-300
+            img
           "
-        >
+          src="../assets/images/pizza-bg.png"
+          alt=""
+        />
+
+        <header id="header" class="m-w-top">
+          <h1 class="font-semibold text-6xl animation1">
+            Newest, freshest and best pizza in
+            <span class="text-red-500">Kortrijk</span>.
+          </h1>
+          <h2 class="font-medium text-2xl pt-10 animation1">
+            Always wanted to try the freshest pizza in the whole wide world?
+            Wait no longer and taste for
+            <span class="text-red-500">youself</span>.
+          </h2>
+          <div class="flex flex-row items-center space-x-5 mt-6 animation1">
+            <button
+              class="
+                rounded-xl
+                bg-yellow-300
+                py-4
+                w-40
+                text-white
+                font-semibold
+              "
+              @click="handleDelivery"
+            >
+              DELIVERY
+            </button>
+            <p class="font-semibold">OR</p>
+            <button
+              class="
+                rounded-xl
+                bg-yellow-300
+                py-4
+                w-40
+                text-white
+                font-semibold
+              "
+              @click="handleCarryout"
+            >
+              CARRYOUT
+            </button>
+          </div>
+        </header>
+        <div class="m-w-landing" id="services">
           <div
             class="
-              m-w-card
-              flex flex-col
-              justify-between
-              items-center
-              bg-white
-              rounded-2xl
-              p-6
-              space-y-4
-              text-center
-              cursor-default
-              lg:justify-self-start
-              animation2
+              grid grid-cols-1
+              lg:grid-cols-2
+              gap-y-10
+              justify-items-stretch
+              pt-24
             "
           >
-            <h2 class="text-xl font-medium">
-              New but <span class="text-p-red">improved</span>
-            </h2>
-            <p class="font-light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi
-            </p>
-          </div>
-          <div
-            class="
-              m-w-card
-              flex flex-col
-              justify-between
-              items-center
-              bg-white
-              rounded-2xl
-              p-6
-              space-y-4
-              text-center
-              cursor-default
-              lg:justify-self-end
-              animation2
-            "
-          >
-            <h2 class="text-xl font-medium">
-              Always <span class="text-p-red">fresh</span>
-            </h2>
-            <p class="font-light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi
-            </p>
-          </div>
-          <div
-            class="
-              m-w-card
-              flex flex-col
-              justify-between
-              items-center
-              bg-white
-              rounded-2xl
-              p-6
-              space-y-4
-              text-center
-              cursor-default
-              lg:justify-self-start
-              animation3
-            "
-          >
-            <h2 class="text-xl font-medium">
-              <span class="text-p-red">Fastest</span> in Kortrijk
-            </h2>
-            <p class="font-light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi
-            </p>
-          </div>
-          <div
-            class="
-              m-w-card
-              flex flex-col
-              justify-between
-              items-center
-              bg-white
-              rounded-2xl
-              p-6
-              space-y-4
-              text-center
-              cursor-default
-              lg:justify-self-end
-              animation3
-            "
-          >
-            <h2 class="text-xl font-medium">
-              <span class="text-p-red">Fastest</span> in Kortrijk
-            </h2>
-            <p class="font-light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi
-            </p>
-          </div>
-        </div>
-        <div class="mt-20 mb-20 flex flex-row justify-between items-center">
-          <div class="space-y-3" id="contact">
-            <h1 class="text-p-red font-semibold text-3xl">Any Questions?</h1>
-            <h2 class="font-medium text-2xl">Contact us anytime.</h2>
-          </div>
-          <div
-            class="
-              flex flex-row
-              justify-between
-              bg-white
-              px-8
-              pt-4
-              pb-5
-              rounded-xl
-              items-center
-              mt-4
-            "
-            style="width: 27rem"
-          >
-            <div>
-              <p class="text-p-gray-200 font-medium">SEND US AN EMAIL</p>
-              <p class="text-xl font-medium">info@epizza.com</p>
+            <div
+              class="
+                m-w-card
+                flex flex-col
+                justify-between
+                items-center
+                bg-white
+                rounded-2xl
+                p-6
+                space-y-4
+                text-center
+                cursor-default
+                lg:justify-self-start
+                animation2
+              "
+            >
+              <h2 class="text-xl font-medium">
+                New but <span class="text-p-red">improved</span>
+              </h2>
+              <p class="font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi
+              </p>
             </div>
-            <a href="mailto:info@epizza.com">
-              <svg
-                class="cursor-pointer"
-                xmlns="http://www.w3.org/2000/svg"
-                width="30"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="#7A7A7A"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path
-                  d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
-                ></path>
-                <polyline points="22,6 12,13 2,6"></polyline>
-              </svg>
-            </a>
+            <div
+              class="
+                m-w-card
+                flex flex-col
+                justify-between
+                items-center
+                bg-white
+                rounded-2xl
+                p-6
+                space-y-4
+                text-center
+                cursor-default
+                lg:justify-self-end
+                animation2
+              "
+            >
+              <h2 class="text-xl font-medium">
+                Always <span class="text-p-red">fresh</span>
+              </h2>
+              <p class="font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi
+              </p>
+            </div>
+            <div
+              class="
+                m-w-card
+                flex flex-col
+                justify-between
+                items-center
+                bg-white
+                rounded-2xl
+                p-6
+                space-y-4
+                text-center
+                cursor-default
+                lg:justify-self-start
+                animation3
+              "
+            >
+              <h2 class="text-xl font-medium">
+                <span class="text-p-red">Fastest</span> in Kortrijk
+              </h2>
+              <p class="font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi
+              </p>
+            </div>
+            <div
+              class="
+                m-w-card
+                flex flex-col
+                justify-between
+                items-center
+                bg-white
+                rounded-2xl
+                p-6
+                space-y-4
+                text-center
+                cursor-default
+                lg:justify-self-end
+                animation3
+              "
+            >
+              <h2 class="text-xl font-medium">
+                <span class="text-p-red">Fastest</span> in Kortrijk
+              </h2>
+              <p class="font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi
+              </p>
+            </div>
+          </div>
+          <div class="mt-20 mb-20 flex flex-row justify-between items-center">
+            <div class="space-y-3" id="contact">
+              <h1 class="text-p-red font-semibold text-3xl">Any Questions?</h1>
+              <h2 class="font-medium text-2xl">Contact us anytime.</h2>
+            </div>
+            <div
+              class="
+                flex flex-row
+                justify-between
+                bg-white
+                px-8
+                pt-4
+                pb-5
+                rounded-xl
+                items-center
+                mt-4
+              "
+              style="width: 27rem"
+            >
+              <div>
+                <p class="text-p-gray-200 font-medium">SEND US AN EMAIL</p>
+                <p class="text-xl font-medium">info@epizza.com</p>
+              </div>
+              <a href="mailto:info@epizza.com">
+                <svg
+                  class="cursor-pointer"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#7A7A7A"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path
+                    d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+                  ></path>
+                  <polyline points="22,6 12,13 2,6"></polyline>
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
