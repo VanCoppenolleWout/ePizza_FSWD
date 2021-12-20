8243f48a37c90ba7dc165aa525c6e20a626b020b
Wout VC <wout.van.coppenolle@student.howest.be>
2021-12-21 00:56:52 +0100

    appheader

--- Frontend/ePizza/src/components/AppHeader.vue
+++ Frontend/ePizza/src/components/AppHeader.vue
@@ -268,7 +268,23 @@ export default defineComponent({
         "
       >
         <router-link to="/account">
-          <p>{{ username }}</p>
+          <div class="flex flex-row items-center space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#FFFFFF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+            <p>{{ username }}</p>
+          </div>
         </router-link>
       </li>
     </transition-group>
@@ -551,7 +567,24 @@ export default defineComponent({
         "
       >
         <router-link to="/account">
-          <p>{{ username }}</p>
+          <div class="flex flex-row items-center space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#FFFFFF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+            <p>{{ username }}</p>
+          </div>
+          
         </router-link>
       </li>
     </transition-group>
